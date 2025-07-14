8016986ef6c6a44d0de1a57664d77ae08e20f46d
sherman07 <shermanzheng8@gmail.com>
2025-07-14 23:54:08 +1200

    Adding arrow trap into the tile map

---
+++ Arrows.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Arrows" tilewidth="32" tileheight="96" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="2D Pixel Dungeon Asset Pack/animations/arrow_32*32/arrow_2.png" width="32" height="96"/>
+  <animation>
+   <frame tileid="0" duration="4000"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image source="2D Pixel Dungeon Asset Pack/animations/arrow_32*32/arrow_5.png" width="32" height="96"/>
+ </tile>
+ <tile id="2">
+  <image source="2D Pixel Dungeon Asset Pack/animations/arrow_32*32/arrow_1.png" width="32" height="96"/>
+ </tile>
+ <tile id="3">
+  <image source="2D Pixel Dungeon Asset Pack/animations/arrow_32*32/arrow_4.png" width="32" height="96"/>
+ </tile>
+ <tile id="4">
+  <image source="2D Pixel Dungeon Asset Pack/animations/arrow_32*32/arrow_3.png" width="32" height="96"/>
+ </tile>
+</tileset>
